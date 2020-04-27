5639de1c178b7359810d32a5789a2be7b88daf28
Kasparas <kasparas.urbelis@gmail.com>
2020-04-27 20:38:43 +0300

    Added registration, login

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('connect-flash')
+const app = express()
+
+let sessionOptions = session({
+    secret: "JavaScript HAHAAHA",
+    store: new MongoStore({client: require("./db")}),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
+})
+
+app.use(sessionOptions)
+app.use(flash())
+
+const router = require('./router')
+app.use(express.urlencoded({extended: false})) //adds user submited data on to of req.body object
+app.use(express.json())
+
+app.use(express.static('public'))
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
+app.use('/', router)
+module.exports = app
